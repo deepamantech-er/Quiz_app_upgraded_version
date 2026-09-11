9e36e0f9d6b5576de3080c0fcf43a2fc2e220e6e
Aman Deep Singh <deepamantech@gmail.com>
2026-09-11 19:36:24 +0530

    function is added for random quiz run, Question number system is added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import random
 print("==QUIZ APP 🧑‍🎓==")
 
 def load_quiz():
@@ -17,12 +18,18 @@ def save_quiz():
         json.dump(quiz, file, indent=4)
 
 def add_questions():
-    while True:
+    while True: 
+        try:
+            last_ques = quiz[-1]
+            ques_no = last_ques["Ques.no"] + 1
+        except IndexError:
+            ques_no = 1
+        print("Quesno",ques_no)
         ques = input("Enter your question: ")
-        opt = [input("Enter option a: "),
-                input("Enter option b: "),
-                input("Enter option c: "),
-                input("Enter option d: ")]
+        opt =  {"a":input("Enter option a: "),
+                "b":input("Enter option b: "),
+                "c":input("Enter option c: "),
+                "d":input("Enter option d: ")}
         while True:
             uncheck_ans = input("Enter Your answer between options a - d: ").strip().lower()
             if uncheck_ans == "a" or uncheck_ans =="b" or uncheck_ans == "c" or uncheck_ans == "d":
@@ -32,7 +39,8 @@ def add_questions():
                 print("Enter answer between a - d")
                 continue 
         
-        add_question = {"Question": ques,
+        add_question = {"Ques.no" : ques_no,
+                        "Question": ques,
                         "Options": opt,
                         "Answer": ans}
         quiz.append(add_question)
@@ -64,14 +72,16 @@ def delete_quiz():
             
 def run_quiz():
     score = 0
-    if quiz == []:
+    if not quiz:
         print("first Add a Quiz, there is no Quiz in memory")
     else:
         print("--You are doing Quiz--")
         for question in quiz:
+            print(question["Ques.no"] ,end=") ")
             print(question["Question"])
-            for items in question["Options"]:
-                print(items)
+            for keys, values in question["Options"].items():
+                print(keys, end=") ")
+                print(values)
             while True:
                 answer = input("Enter answer between a - d: ").strip().lower()
                 if answer == "a" or answer == "b" or answer == "c" or answer == "d":
@@ -86,12 +96,83 @@ def run_quiz():
             else:
                 print("answer is wrong")
         print("Quiz has ended") 
-        print(f"You Scored {score}/{len(quiz)}")    
+        print(f"You Scored {score}/{len(quiz)}")   
         
+def random_quiz_run():
+    Quesno = 1
+    score = 0
+    used_questions = []
+    if not quiz:
+        print("first Add a Quiz, there is no Quiz in memory")
+    else:
+        print("--You are doing random quiz--")
+        while True:
+            try:
+                random_question_for_use = [question for question in quiz if question not in used_questions]
+                random_question = random.choice(random_question_for_use)
+            except IndexError:
+                print("Quiz has completed")
+                break 
+            print(f"Ques.no {Quesno}")
+            Quesno += 1 
+            print(random_question["Question"])
+            for keys, values in random_question["Options"].items():
+                print(keys, end=") ")
+                print(values)
+            used_questions.append(random_question)
+            while True:
+                answer = input("Enter answer between a - d: ")
+                if answer == "a" or answer == "b" or answer == "c" or answer == "d":
+                    break 
+                else:
+                    print("Enter a valid answer between a - d")
+                    continue 
+            if answer == random_question["Answer"]:
+                print("answer is correct")
+                score += 1
+                continue
+            else:
+                print("answer is wrong")
+                continue 
+        print(f"Your score is {score}/{len(quiz)}")
+        
+def edit_question():
+    while True:
+        try:
+            choice = int(input("Enter Question no which you want to edit: "))
+        except ValueError:
+            print("Enter a valid choice")
+            continue 
+        for question in quiz:
+            if choice == question["Ques.no"]:
+                print(question["Question"])
+                for keys, values in question["Options"].items():
+                    print(keys, end=") ")
+                    print(values)
+                print(f"Answer is {question["Answer"]}")
+                question["Question"] = input("Enter updated Question: ")
+                question["Options"] = {"a":input("Enter option a: "),
+                                       "b":input("Enter option b: "),
+                                       "c":input("Enter option c: "),
+                                       "d":input("Enter option d: ")}
+                while True:
+                    modified_answer = input("Enter Your answer between options a - d: ").strip().lower()
+                    if modified_answer == "a" or modified_answer == "b" or  modified_answer == "c" or modified_answer == "d":
+                        question["Answer"] = modified_answer 
+                        save_quiz()
+                        print("Question edited successfully")
+                        return  
+                    else:
+                        print("Enter a valid answer between a-d")
+                        continue 
+        else:
+            print("Enter a valid Question no") 
+            
 while True:
     print("Choose Task ?")
     print("1 = Run quiz")
-    print("2 = Quiz settings")
+    print("2 = Run Random quiz")
+    print("3 = Quiz settings")
     try:
         choice = int(input("Enter your choice: "))
     except ValueError:
@@ -102,20 +183,27 @@ while True:
         run_quiz()
         
     elif choice == 2:
+        random_quiz_run()
+        
+    elif choice == 3:
         while True:
             print("--SETTINGS--")
             print("-------------")
             print("1. Add Questions")
-            print("2. Delete Quiz")
+            print("2. Edit Question")
+            print("3. Delete Quiz")
             try:
-                choice = int(input("Enter Your choice between 1 - 2 : "))
+                choice = int(input("Enter Your choice between 1 - 3 : "))
             except ValueError:
                 print("Enter a valid input between 1 - 2")
-                continue
+                continue          
             if choice == 1:
                 add_questions()
                 break 
             elif choice == 2:
+                edit_question()
+                break 
+            elif choice == 3:
                 delete_quiz()
                 break 
             else:
